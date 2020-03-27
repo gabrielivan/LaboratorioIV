01a8dc9fc3ea1e70a27277f144823e44713e098c
Gabriel <gabrielivansaliba@gmail.com>
2020-03-26 22:07:41 -0300

    CLASE 01

---
+++ CLASE 01/myApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+ 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ErrorComponent } from './componentes/error/error.component';
+ 
+@NgModule({
+  declarations: [
+    AppComponent,
+    BienvenidaComponent,
+    LoginComponent,
+    ErrorComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
