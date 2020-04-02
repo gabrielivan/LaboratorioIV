7e8a12e72e1e4bb82726cb50519d1ebbe7968780
Gabriel <gabrielivansaliba@gmail.com>
2020-04-02 19:45:10 -0300

    clase2 primer parte

--- CLASE 02/myApp/src/app/app-routing.module.ts
+++ CLASE 02/myApp/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ErrorComponent } from './componentes/error/error.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bienvenida', component: BienvenidaComponent},
+  {path: '', component: LoginComponent},
+  {path: '**', component: ErrorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
