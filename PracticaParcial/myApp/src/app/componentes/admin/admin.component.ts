c0e77df1c99ba586f5a5ee1995c80e7cf0697e88
Gabriel <gabrielivansaliba@gmail.com>
2020-04-22 00:08:07 -0300

    localStorage

--- PracticaParcial/myApp/src/app/componentes/admin/admin.component.ts
+++ PracticaParcial/myApp/src/app/componentes/admin/admin.component.ts
@@ -13,20 +13,24 @@ export class AdminComponent implements OnInit {
 
   constructor() { 
     this.ListadoProductosPrincipal= [
-      { Id: 1, Descripcion: "Flexible-180x220cm" , Precio: 1000, FechaDeVencimiento: new Date("2020/04/17"), RutaDeFoto: "Descktop.jpg", Tipo:  0},
-      { Id: 2, Descripcion: "Fijo-190x230" , Precio: 2500, FechaDeVencimiento: new Date("2020/04/16"), RutaDeFoto: "Descktop.jpg", Tipo: 1}
+      // { Id: 1, Descripcion: "Flexible-180x220cm" , Precio: 1000, FechaDeVencimiento: new Date("2020/04/17"), RutaDeFoto: "Descktop.jpg", Tipo:  0},
+      // { Id: 2, Descripcion: "Fijo-190x230" , Precio: 2500, FechaDeVencimiento: new Date("2020/04/16"), RutaDeFoto: "Descktop.jpg", Tipo: 1}
     ];
   }
 
   ngOnInit(): void {
-    
+    var auxListado = localStorage.getItem('listado');
+    if(auxListado)
+    {
+      this.ListadoProductosPrincipal = JSON.parse(auxListado);
+    }
   }
   
   mostrarProducto(parametroProducto: Producto)
   {
       console.info("producto",parametroProducto);
       this.ListadoProductosPrincipal.push(parametroProducto);
-  }
+      this.saveChangesInLocalStorage();  }
 
   tomarProductoParaDetalles(NuevoProducto: Producto)
   {
@@ -38,6 +42,11 @@ export class AdminComponent implements OnInit {
     const index = this.ListadoProductosPrincipal.indexOf(productoABorrar, 0);
     if (index > -1) {
       this.ListadoProductosPrincipal.splice(index, 1);
+      this.saveChangesInLocalStorage();
     }
+  }
+
+  saveChangesInLocalStorage(){
+    localStorage.setItem('listado', JSON.stringify(this.ListadoProductosPrincipal));
   }
 }
