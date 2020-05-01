ef1132b7883b2d21aa56133938e737f45077948b
Gabriel <gabrielivansaliba@gmail.com>
2020-04-30 21:09:41 -0300

    agregar firebase corregir traer peliculas y aplicar img

--- Simulacro-Parcial1/myApp/src/app/peliculas/tabla-pelicula/tabla-pelicula.component.ts
+++ Simulacro-Parcial1/myApp/src/app/peliculas/tabla-pelicula/tabla-pelicula.component.ts
@@ -15,6 +15,14 @@ export class TablaPeliculaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var a = [
+      {Id: 1, Nombre: "la guerra de las galaxias", FechaDeEstreno: "15/11/2020", CantidadDePublico: 40, FotoDePelicula: "../../assets/descarga.jpg", Tipo: "Otros"},
+    {Id: 2, Nombre: "matrix4", FechaDeEstreno: "10/10/2020", CantidadDePublico: 60, FotoDeLaPelicula: "../../assets/descarga.jpg", Tipo: "Otros"},
+    {Id: 3, Nombre: "Rapido y furioso 9", FechaDeEstreno: "20/12/2020", CantidadDePublico: 70, FotoDeLaPelicula: "../../assets/rapidosyfuriosos9.jpg", Tipo: "Otros"}];
+
+    var b = JSON.stringify(a);
+    localStorage.setItem("listado", b);
+
     var auxListado = localStorage.getItem('listado');
     if(auxListado)
     {
